aa74626075394ee30a444be4b71c5f4c6a5c0d99
Sameer Laxman PIse <79313336+sameerpise@users.noreply.github.com>
2025-02-16 19:53:30 +0530

    Update Hero.jsx

--- blog/src/Componets/Hero.jsx
+++ blog/src/Componets/Hero.jsx
@@ -3,11 +3,12 @@ import Card from "./Card/Card";
 import { ParallaxProvider, Parallax } from "react-scroll-parallax";
 import "../Componets/animatedline/animeted.css";
 import "./hero.css"
+import bgImage from "../assets/images/head.jpg";
 export default function Hero() {
   return (
     <ParallaxProvider>
       {/* Hero Section */}
-      <Parallax speed={-10} className="hero-section ">
+      <Parallax speed={-10} className="hero-section " style={{ backgroundImage: `url(${bgImage})` }}>
         <div className="wrapintro text-center">
           <h1 className="animated-underline">Welcome to Sameer Writes</h1>
           <h2 className="lead">
