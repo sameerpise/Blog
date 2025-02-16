5ea849da7283b8e36e83a1c7b945c57c4652facb
Sameer Laxman PIse <79313336+sameerpise@users.noreply.github.com>
2025-02-16 19:23:11 +0530

    Update Login.jsx

--- blog/src/Componets/Auth/Login.jsx
+++ blog/src/Componets/Auth/Login.jsx
@@ -15,7 +15,7 @@ const Login = () => {
         setError("");
 
         try {
-            const response = await fetch("http://localhost:5000/auth/login", {
+            const response = await fetch("https://blog-maxs.onrender.com/auth/login", {
                 method: "POST",
                 headers: { "Content-Type": "application/json" },
                 body: JSON.stringify(form),
